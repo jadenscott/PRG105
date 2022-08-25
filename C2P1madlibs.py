a4dbec66c5bb603fde73190c6c87d76aca7df478
jadenscott <111458082+jadenscott@users.noreply.github.com>
2022-08-24 20:27:18 -0500

    Add files via upload

---
+++ C2P1madlibs.py
@@ -0,0 +1,25 @@
+# Madlibs game using the lyrics to Sad But True by Metallica
+
+greeting = input("Enter a greeting: ")
+noun_one = input("Enter a noun: ")
+verb = input("Enter a verb in its third person singular form (ends in 's'): ")
+noun_two = input("Enter another noun: ")
+adjective_one = input("Enter an adjective: ")
+noun_three = input("Enter another noun: ")
+emotion = input("Enter an emotion: ")
+adjective_two = input("Enter another adjective: ")
+
+print(f"{greeting} ({greeting}), I'm your {noun_one}, I'm the one who {verb} you there")
+print(f"{greeting} ({greeting}), I'm your {noun_one}, I'm the one who {verb}")
+print(f"They (They), they betray, I'm your only true {noun_one} now")
+print(f"They (They), they'll betray, I'm forever there \n")
+
+print(f"I'm your {noun_two}, make you {adjective_one}")
+print(f"I'm your {noun_two} when you must steal")
+print(f"I'm your {noun_two} when you can't feel")
+print(f"{emotion} but {adjective_two} \n")
+
+print(f"I'm your {noun_three}, mind {adjective_one}")
+print(f"I'm your {noun_three} while you're away")
+print(f"I'm your {noun_three} while you repay")
+print(f"You know it's {emotion} but {adjective_two}")
