e78d21ccc6caca4fca6f226b83476e452617a0d5
jadenscott <111458082+jadenscott@users.noreply.github.com>
2022-09-13 20:29:33 -0500

    Add files via upload
    Final version, has been corrected

--- C4assessrepetition.py
+++ C4assessrepetition.py
@@ -18,11 +18,14 @@ print("This projection assumes a 3% annual raise and a 6% annual return on inves
 print(f'{"Year":10}{"Income":10}{"Savings Contribution":10}\t{"Total Savings":10}')
 
 # Year 1 data is printed
-print(f'1\t\t{annual_income:,.2f}\t\t{savings_contr:,.2f}\t\t\t{total_savings + savings_contr + (total_savings * .06):10,.2f}')
+print(f'1\t\t{annual_income:9,.0f}\t\t{savings_contr:8,.0f}\t\t\t{total_savings + savings_contr + (total_savings * .06):10,.0f}')
+
+# total_savings variable is redefined
+total_savings = total_savings + savings_contr + (total_savings * .06)
 
 # For loop accounts for an annual 3% raise, adjusted savings contribution, and a 6% return on investment for total savings
 for year in range(2, difference):
     annual_income += annual_income * .03
     savings_contr = annual_income * (percent_income / 100)
     total_savings += savings_contr + (total_savings * .06)
-    print(f'{year}\t\t{annual_income:,.2f}\t\t{savings_contr:,.2f}\t\t\t{total_savings:,.2f}')
+    print(f'{year}\t\t{annual_income:9,.0f}\t\t{savings_contr:8,.0f}\t\t\t{total_savings:10,.0f}')
