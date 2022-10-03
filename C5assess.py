c9de33911b2a9f35cbbe7d38034b4d6ca8625ddc
jadenscott <111458082+jadenscott@users.noreply.github.com>
2022-10-03 13:57:19 -0500

    Add files via upload
    revised

--- C5assess.py
+++ C5assess.py
@@ -7,38 +7,35 @@ PI = 3.14159265358  # global variable for pi is declared
 
 
 def main():
-    menu()  # initially, shape options are printed
-    user_num = int(input("Please enter the number of your selection: "))
-    user_num = validate(user_num)  # to ensure that user_num is an integer between 1 and 5
-    if user_num == 1:
-        base = int(input("Enter the base of the rectangle in cm: "))
-        height = int(input("Enter the height of the rectangle in cm: "))
-        rectangle(base, height)
-        user_num = int(input("Please enter the number of your selection: "))
-        user_num = validate(user_num)
-    if user_num == 2:
-        base = int(input("Enter the base of the triangle in cm: "))
-        height = int(input("Enter the height of the triangle in cm: "))
-        triangle(base, height)
-        user_num = int(input("Please enter the number of your selection: "))
-        user_num = validate(user_num)
-    if user_num == 3:
-        length = int(input("Enter the length of one side of the square in cm: "))
-        square(length)
-        user_num = int(input("Please enter the number of your selection: "))
-        user_num = validate(user_num)
-    if user_num == 4:
-        radius = int(input("Enter the radius of the circle in cm: "))
-        circle(radius)
-        user_num = int(input("Please enter the number of your selection: "))
-        user_num = validate(user_num)
-    if user_num == 5:  # upon quitting, "Goodbye!" will be printed
-        print("Goodbye!")
+    # choice is initialized
+    user_num = 0
+    while user_num != 5:
+        print("This program will find the area of a shape for you.\n1. Rectangle\n2. Triangle\n3. Square\n4. Circle\n"
+            "5. Quit")  # menu choices are printed
+        user_num = int(input("Please enter the number of your selection: "))  # get user's choice
+        user_num = validate(user_num)  # to ensure that user_num is an integer between 1 and 5
 
-
-def menu():
-    print("This program will find the area of a shape for you.\n1. Rectangle\n2. Triangle\n3. Square\n4. Circle\n"
-          "5. Quit")
+        # if-elif statements to process user input
+        if user_num == 1:
+            base = int(input("Enter the base of the rectangle in cm: "))  # get base in cm
+            height = int(input("Enter the height of the rectangle in cm: "))  # get height in cm
+            area = rectangle(base, height)
+            print(f"The area of the rectangle is {area:.2f} cm squared")  # print area of rectangle
+        elif user_num == 2:
+            base = int(input("Enter the base of the triangle in cm: "))  # get base in cm
+            height = int(input("Enter the height of the triangle in cm: "))  # get height in cm
+            area = triangle(base, height)
+            print(f"The area of the triangle is {area:.2f} cm squared")  # print area of triangle
+        elif user_num == 3:
+            length = int(input("Enter the length of one side of the square in cm: "))  # get length of one side in cm
+            area = square(length)
+            print(f"The area of the square is {area:.2f} cm squared")  # print area of square
+        elif user_num == 4:
+            radius = int(input("Enter the radius of the circle in cm: "))  # get radius of circle in cm
+            area = circle(radius)
+            print(f"The area of the circle is {area:.2f} cm squared")  # print area of circle
+        elif user_num == 5:  # upon quitting, "Goodbye!" will be printed
+            print("Goodbye!")
 
 
 def validate(num):
@@ -48,32 +45,25 @@ def validate(num):
     return num
 
 
-def rectangle(num1, num2):  # takes the base and the height and calculates area
-    area = num1 * num2
-    print(f"The area of the rectangle is {area:.2f} cm squared")
-    print('')
-    menu()
+def rectangle(base, height):  # calculates and returns area of a rectangle
+    area = base * height
+    return area
 
 
-def triangle(num1, num2):  # takes the base and the height and calculates area
-    area = 1/2 * num1 * num2
-    print(f"The area of the triangle is {area:.2f} cm squared")
-    print('')
-    menu()
+def triangle(base, height):  # calculates and returns area of a triangle
+    area = (1/2) * base * height
+    return area
 
 
-def square(num):  # takes the length of any side and calculates area
-    area = num**2
-    print(f"The area of the square is {area:.2f} cm squared")
-    print('')
-    menu()
+def square(length):  # calculates and returns area of a square
+    area = length**2
+    return area
 
 
-def circle(num):  # takes the radius and calculates area
-    area = PI * (num**2)
-    print(f"The area of the circle is {area:.2f} cm squared")
-    print('')
-    menu()
+def circle(radius):  # calculates and returns area of a circle
+    area = PI * (radius**2)
+    return area
 
 
-main()
+if __name__ == '__main__':
+    main()
