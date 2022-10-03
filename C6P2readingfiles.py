6de1883cce7d0032ae2e2b8e806fdaea031b2dc8
jadenscott <111458082+jadenscott@users.noreply.github.com>
2022-10-03 12:02:46 -0500

    Add files via upload
    revised

--- C6P2readingfiles.py
+++ C6P2readingfiles.py
@@ -8,9 +8,6 @@ def main():
     # opens 'c6p2numbers.txt' for reading and creates numbers_file variable
     numbers_file = open('c6p2numbers.txt', 'r')
 
-    # line variable reads a line from numbers_file
-    line = numbers_file.readline()
-
     # initializes accumulator
     total = 0.0
 
@@ -21,19 +18,16 @@ def main():
     average = 0.0
 
     for line in numbers_file:
-        # strips new line character
-        line = line.rstrip('\n')
+        # converts to float and strips new line character
+        value = float(line.rstrip('\n'))
 
-        # converts each number into a float
-        value = float(line)
-
-        # prints each value as a float
+        # prints each value line-by-line
         print(f'{value:,.2f}')
 
         # calculates total, count and average
         total += value
         count += 1
-        average = total / (count + 1)
+        average = total / count
 
     print('')
 
@@ -42,7 +36,7 @@ def main():
 
     # prints total, number of entries and average
     print(f'Total:{total:20,.2f}')
-    print(f'Number of entries:{count + 1:8}')
+    print(f'Number of entries:{count:8}')
     print(f'Average:{average:18,.2f}')
 
 
