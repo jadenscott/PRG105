3fddb2a4725903e7c4c1c00522e64c6a3cf35924
jadenscott <111458082+jadenscott@users.noreply.github.com>
2022-10-04 14:49:15 -0500

    Add files via upload

--- ch6_assess.py
+++ ch6_assess.py
@@ -1,6 +1,6 @@
 """
-This program displays each number from a text file, then, adds them all up and displays the total, then, displays how
-many numbers there are, then, calculate and display the average.
+This program tries to open a given file, then tries to print the numerical data of that file.
+Then, the program calculates and prints the total, number of entries and average.
 """
 
 
@@ -13,16 +13,6 @@ def main():
     # tries to open the user's file
     try:
         infile = open(user_file, 'r')
-        infile.close()
-
-    # prints an error message if the user does not type "sales_error-1"
-    except IOError:
-        if user_file != "sales_error-1":
-            print(f"Unable to access the file: '{user_file}'\n")
-
-    if user_file == "sales_error-1":
-        # opens 'sales_error-1.txt' for reading and creates infile variable
-        infile = open('sales_error-1.txt', 'r')
 
         # initializes accumulator
         total = 0.0
@@ -30,35 +20,39 @@ def main():
         # initializes count variable
         count = 0
 
-        # initializes average variable
-        average = 0.0
-
         for line in infile:
             try:
                 # converts to float and strips new line character
                 value = float(line.rstrip('\n'))
 
-            # prints error message to account for bad data
+                # calculates total and count
+                total += value
+                count += 1
+
+                # prints each value line-by-line
+                print(f'{value:,.2f}')
+
+                # prints error message to account for bad data
             except ValueError as err:
                 print(err)
+                # print(f"Line {count + 1} with a value {infile.readline(count)} was invalid.")
 
-            # prints each value line-by-line
-            print(f'{value:,.2f}')
-
-            # calculates total, count and average
-            total += value
-            count += 1
-            average = total / count
-
-        print('')
+        # calculates average
+        average = total / count
 
         # closes file
         infile.close()
+
+        print('')
 
         # prints total, number of entries and average
         print(f'Total:{total:20,.2f}')
         print(f'Number of entries:{count:8}')
         print(f'Average:{average:18,.2f}')
+
+    # prints an error message if the user's file cannot be read
+    except IOError:
+        print(f"Unable to access the file: '{user_file}'")
 
 
 if __name__ == '__main__':
