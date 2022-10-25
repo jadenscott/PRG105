edd9d41e60afb74bd683e40b210df4149d627571
jadenscott <111458082+jadenscott@users.noreply.github.com>
2022-10-25 15:08:24 -0500

    Add files via upload

--- ch_10_exercises.py
+++ ch_10_exercises.py
@@ -22,19 +22,8 @@ class Dice:  # note class names are capitalized
     def roll(self):
         # TODO get a random value and set side_up for the 6 sides of the dice
         # Note: Think about what makes sense for dice compared to a coin toss
-        if random.randint(1, 6) == 1:
-            self.side_up = 1
-        elif random.randint(1, 6) == 2:
-            self.side_up = 2
-        elif random.randint(1, 6) == 3:
-            self.side_up = 3
-        elif random.randint(1, 6) == 4:
-            self.side_up = 4
-        elif random.randint(1, 6) == 5:
-            self.side_up = 5
-        else:
-            self.side_up = 6
-            
+        self.side_up = random.randint(1, 6)
+
     def get_side_up(self):
         return self.side_up
     
