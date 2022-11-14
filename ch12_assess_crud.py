c26b0eb3f3bf77c6e94d18a9bd4f3d65e6087725
jadenscott <111458082+jadenscott@users.noreply.github.com>
2022-11-14 14:18:23 -0600

    Add files via upload

--- ch12_assess_crud.py
+++ ch12_assess_crud.py
@@ -13,16 +13,13 @@ QUIT = 5
 
 
 def main():
-    # initialize dictionary
-    customer_file = {}
-
     try:
         # pickle dictionary
         infile = open('customer_file.dat', 'rb')
-        pickle.load(infile)
+        customer_file = pickle.load(infile)
         infile.close()
 
-    except EOFError:
+    except IOError:
         # initialize dictionary
         customer_file = {}
 
@@ -31,17 +28,17 @@ def main():
 
     # calls appropriate function passing customer_file as an argument
     while choice != QUIT:
-        menu()
-    if choice == LOOK_UP:
-        read(customer_file)
-    elif choice == ADD:
-        create(customer_file)
-    elif choice == CHANGE:
-        update(customer_file)
-    elif choice == DELETE:
-        delete(customer_file)
-    else:
-        print('Goodbye!')
+        choice = menu()
+        if choice == LOOK_UP:
+            read(customer_file)
+        elif choice == ADD:
+            create(customer_file)
+        elif choice == CHANGE:
+            update(customer_file)
+        elif choice == DELETE:
+            delete(customer_file)
+        else:
+            print('Shutting down...')
 
 
 def menu():
@@ -49,7 +46,7 @@ def menu():
     choice = 0
 
     # keeps displaying menu if choice is not valid
-    while choice != 1 or 2 or 3 or 4 or 5:
+    while choice != 1 and choice != 2 and choice != 3 and choice != 4 and choice != 5:
         print("Enter your selection:\n\t1. Look up an email by name\n\t2. Add a new entry\n\t"
               "3. Change a person's email\n\t4. Delete an entry\n\t5. Quit")
 
@@ -63,17 +60,17 @@ def menu():
 
 def read(customer_file):
     # get name from user
-    customer_name = input('Enter a name: ')
+    customer_name = input('\nEnter a name: ')
 
     # if the name is found, the corresponding email is printed
     if customer_name in customer_file:
-        print(customer_file[customer_name])
+        print(f'{customer_file[customer_name]}\n')
     else:
-        print('No data found. Try adding a new entry.')
+        print('No data found. Try adding a new entry.\n')
 
 
 def create(customer_file):
-    customer_name = input('Enter a name: ')
+    customer_name = input('\nEnter a name: ')
 
     # get email address from user
     customer_email = input(f'Enter an email address for {customer_name}: ')
@@ -81,15 +78,14 @@ def create(customer_file):
     # adds a key-value pair to the dictionary customer_file
     customer_file[customer_name] = customer_email
 
-    infile = open('customer_file.dat', 'wb')
-    pickle.dump(customer_file, infile)
-    infile.close()
+    # pickles data
+    save(customer_file)
 
-    print(f'{customer_name} has been added with email {customer_email}')
+    print(f'{customer_name} has been added with email {customer_email}\n')
 
 
 def update(customer_file):
-    customer_name = input('Enter a name: ')
+    customer_name = input('\nEnter a name: ')
 
     # nested if statements to replace a customer's email address
     if customer_name in customer_file:
@@ -98,20 +94,17 @@ def update(customer_file):
         if answer == 'y':
             customer_email = input(f'Enter the new email address for {customer_name}: ')
             customer_file[customer_name] = customer_email
+            save(customer_file)
 
-            infile = open('customer_file.dat', 'wb')
-            pickle.dump(customer_file, infile)
-            infile.close()
-
-            print(f'The email for {customer_name} is now {customer_email}')
+            print(f'The email for {customer_name} is now {customer_email}\n')
         else:
-            print('No changes were made.')
+            print('No changes were made.\n')
     else:
-        print('No data found. Try adding a new entry.')
+        print('No data found. Try adding a new entry.\n')
 
 
 def delete(customer_file):
-    customer_name = input('Enter a name: ')
+    customer_name = input('\nEnter a name: ')
 
     # nested if statements to delete a customer's entry
     if customer_name in customer_file:
@@ -119,16 +112,12 @@ def delete(customer_file):
                        f' delete it? (y/n) ')
         if answer == 'y':
             del customer_file[customer_name]
-
-            infile = open('customer_file.dat', 'wb')
-            pickle.dump(customer_file, infile)
-            infile.close()
-
-            print(f'{customer_name} has been removed')
+            save(customer_file)
+            print(f'{customer_name} has been removed\n')
         else:
-            print('No changes were made.')
+            print('No changes were made.\n')
     else:
-        print('No data found. Try adding a new entry.')
+        print('No data found. Try adding a new entry.\n')
 
 
 def save(customer_file):
@@ -136,7 +125,7 @@ def save(customer_file):
         infile = open('customer_file.dat', 'wb')
         pickle.dump(customer_file, infile)
         infile.close()
-    except EOFError:
+    except IOError:
         print('Error: unable to save file')
 
 
